0da5d969607db73c9fdc7028df2be6a5a24b859b
HazalGultekin <hazallgultekin@gmail.com>
2023-04-16 20:16:47 +0300

    use map method for RDD transformation

---
+++ src/main/java/com/bigdatacompany/spark/model/Person.java
@@ -0,0 +1,49 @@
+package com.bigdatacompany.spark.model;
+
+public class Person {
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String gender;
+    private String country;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
