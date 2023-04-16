c877926a917fc9e005ebc6f08abb448a58d1c0f7
HazalGultekin <hazallgultekin@gmail.com>
2023-04-16 22:45:30 +0300

    fixed problems

--- src/main/java/com/bigdatacompany/spark/MapTrans.java
+++ src/main/java/com/bigdatacompany/spark/MapTrans.java
@@ -3,16 +3,18 @@ package com.bigdatacompany.spark;
 import com.bigdatacompany.spark.model.Person;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 
-import java.util.function.Function;
+
+
 
 public class MapTrans {
     public static void main(String[] args) {
         System.setProperty("hadoop.home.dir", "C:\\hadoop-common-2.2.0-bin-master");
         JavaSparkContext javaSparkContext=new JavaSparkContext("local","Map Transformation Spark");
 
-        JavaRDD<String> rawdata = javaSparkContext.textFile("\"C:\\Users\\HAZAL\\OneDrive\\Masaüstü\\person.csv");
+        JavaRDD<String> rawdata = javaSparkContext.textFile("C:\\Users\\HAZAL\\OneDrive\\Masaüstü\\person.csv");
 
         JavaRDD<Person> loadPerson = rawdata.map(new Function<String, Person>() {
             public Person call(String line) throws Exception{
@@ -28,7 +30,7 @@ public class MapTrans {
         });
 
         loadPerson.foreach(new VoidFunction<Person>() {
-            @Override
+
             public void call(Person person) throws Exception {
                 System.out.println("Adı : "+ person.getFirst_name()+" Soyadı: "+person.getLast_name());
             }
