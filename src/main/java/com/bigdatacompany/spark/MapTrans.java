24e3d300023c5bff4a71d04354a2cc254229e4f8
HazalGultekin <hazallgultekin@gmail.com>
2023-04-17 21:59:06 +0300

    learn lazy evaluation

--- src/main/java/com/bigdatacompany/spark/MapTrans.java
+++ src/main/java/com/bigdatacompany/spark/MapTrans.java
@@ -41,11 +41,21 @@ public class MapTrans {
                 p.setEmail(data[2]);
                 p.setGender(data[3]);
                 p.setCountry(data[4]);
+                p.setAge(Integer.parseInt(data[5]));
+                System.out.println(p.getFirst_name());
                 return p;
             }
         });
 
+<<<<<<< Updated upstream
         JavaPairRDD<String, String> pairRdd = loadPerson.mapToPair(new PairFunction<Person, String, String>() {
+=======
+        System.out.println(loadPerson.count());
+
+
+         //pairRDD
+        /*JavaPairRDD<String, String> pairRdd = loadPerson.mapToPair(new PairFunction<Person, String, String>() {
+>>>>>>> Stashed changes
             @Override
             public Tuple2<String, String> call(Person person) throws Exception {
                 return new Tuple2<String, String>(person.getEmail(), person.getCountry());
@@ -57,7 +67,28 @@ public class MapTrans {
             public void call(Tuple2<String, String> data) throws Exception {
                 System.out.println("Key : "+ data._1+" -- Value : "+data._2);
             }
+<<<<<<< Updated upstream
         });
+=======
+        }); */
+
+
+        // groupByKey
+       /* JavaPairRDD<String, Person> pairRdd = loadPerson.mapToPair(new PairFunction<Person, String, Person>() {
+            @Override
+            public Tuple2<String, Person> call(Person person) throws Exception {
+                return new Tuple2<String, Person>(person.getCountry(), person);
+            }
+        });
+
+        JavaPairRDD<String, Iterable<Person>> groupedData = pairRdd.groupByKey();
+        groupedData.foreach(new VoidFunction<Tuple2<String, Iterable<Person>>>() {
+            @Override
+            public void call(Tuple2<String, Iterable<Person>> data) throws Exception {
+                System.out.println("Key : "+data._1+" Count : "+ Iterables.size(data._2));
+            }
+        }); */
+>>>>>>> Stashed changes
 
 
         // Foreach ile ekrana yazdırma
