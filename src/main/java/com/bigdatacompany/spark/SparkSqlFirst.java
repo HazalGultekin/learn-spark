904026987063bce144ce0bfa8069a011a487dd4c
HazalGultekin <hazallgultekin@gmail.com>
2023-04-19 00:07:52 +0300

    made an example about reading csv

--- src/main/java/com/bigdatacompany/spark/SparkSqlFirst.java
+++ src/main/java/com/bigdatacompany/spark/SparkSqlFirst.java
@@ -8,12 +8,9 @@ public class SparkSqlFirst {
     public static void main(String[] args) {
         System.setProperty("hadoop.home.dir", "C:\\hadoop-common-2.2.0-bin-master");
         SparkSession sparkSession = SparkSession.builder().master("local").appName("First Exam").getOrCreate();
-        Dataset<Row> rawDS = sparkSession.read().csv("C:\\Users\\HAZAL\\OneDrive\\Masaüstü\\person.csv");
+        Dataset<Row> rawDS = sparkSession.read().option("header",true).csv("C:\\Users\\HAZAL\\OneDrive\\Masaüstü\\person.csv");
 
-       // rawDS.show();
-        // rawDS.printSchema();
-
-        Dataset<Row> selectDS = rawDS.select("_c0", "_c3");
-        selectDS.show();
+        Dataset<Row> wcDS = rawDS.withColumn("firstNameTest", rawDS.col("firstName"));
+        wcDS.show();
     }
 }
