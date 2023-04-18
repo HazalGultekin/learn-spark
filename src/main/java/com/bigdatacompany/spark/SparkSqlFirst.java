f17adb87df442083899e1a06a88d99a1cd4c03d3
HazalGultekin <hazallgultekin@gmail.com>
2023-04-19 01:00:47 +0300

    sql structure

--- src/main/java/com/bigdatacompany/spark/SparkSqlFirst.java
+++ src/main/java/com/bigdatacompany/spark/SparkSqlFirst.java
@@ -3,14 +3,24 @@ package com.bigdatacompany.spark;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
 
 public class SparkSqlFirst {
     public static void main(String[] args) {
         System.setProperty("hadoop.home.dir", "C:\\hadoop-common-2.2.0-bin-master");
-        SparkSession sparkSession = SparkSession.builder().master("local").appName("First Exam").getOrCreate();
-        Dataset<Row> rawDS = sparkSession.read().option("header",true).csv("C:\\Users\\HAZAL\\OneDrive\\Masaüstü\\person.csv");
 
-        Dataset<Row> wcDS = rawDS.withColumn("firstNameTest", rawDS.col("firstName"));
-        wcDS.show();
+        StructType schema = new StructType().add("firstName", DataTypes.StringType)
+                .add("lastName", DataTypes.StringType)
+                .add("email", DataTypes.StringType)
+                .add("gender", DataTypes.StringType)
+                .add("country", DataTypes.StringType);
+
+        SparkSession sparkSession = SparkSession.builder().master("local").appName("First Exam").getOrCreate();
+        Dataset<Row> rawDS = sparkSession.read().option("header",true).schema(schema).csv("C:\\Users\\HAZAL\\OneDrive\\Masaüstü\\person.csv");
+
+        Dataset<Row> selDS = rawDS.select("firstName", "email", "country");
+        Dataset<Row> chinaDS = selDS.filter(selDS.col("country").equalTo("China"));
+
     }
 }
